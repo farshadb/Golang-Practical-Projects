b278ad37b0bda09eb570cb10f1688019dedade6b
F4r5h4d <bolouri.farshad@gmail.com>
2022-07-21 19:04:31 +0430

    commet added to code.

--- 02_MutexSoloution/main.go
+++ 02_MutexSoloution/main.go
@@ -15,14 +15,20 @@ func increment(wg *sync.WaitGroup, m *sync.Mutex) {
 }
 
 func main () {
+
 	var w sync.WaitGroup
+
+	// m is variable form sync.Mutex type
 	var m sync.Mutex
 
 	// This loop will create 1000 goroutines
 	for i :=0; i < 1000; i++ {
 		w.Add(1)
+		// here we pass  both wg and m to increment function to ensoure that only one goroutine will be accessing x at a time
+		//and in this case only one goroutine will be accessing x at a time and x is lock until that goroutine is done
 		go increment(&w, &m)
 	}
+	// wait for all goroutines to finish
 	w.Wait()
 	fmt.Println("final value of x", x)
 }
