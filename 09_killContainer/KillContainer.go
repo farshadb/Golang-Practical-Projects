7042329cd1c421287b09a494654656c1aef33174
F4r5h4d <bolouri.farshad@gmail.com>
2022-07-22 20:36:41 +0430

    kill container added.

---
+++ 09_killContainer/KillContainer.go
@@ -0,0 +1,37 @@
+package killContainer
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func killContainer (fileName string) error {
+	data, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return err
+	}
+	
+	defer os.Remove(fileName)
+	cid := strings.TrimSpace(string(data))
+
+	if !isValidID(cid) {
+		return fmt.Errorf("bad container id: %q", cid)
+	}
+
+	log.Printf("Something container %s", cid)
+
+	if err := exec.Command("docker", "rm", "-f", cid).Run(); err != nil {
+		return fmt.Errorf("Failed to stop container %s: %v", cid, err)
+	}
+		return nil
+}
+
+func isValidID(cid string) bool {
+	return len(cid) == 12 || len(cid) == 64 	
+
+}
